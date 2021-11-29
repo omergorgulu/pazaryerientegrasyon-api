740cdc63e607336b521b6aadc0418a27630bd044
Ali Emrah PEKESEN <aliemrahpekesen@gmail.com>
2021-11-29 14:25:05 +0300

    first commit

---
+++ src/main/java/com/example/pazaryerientegrasyon/PazarYeriEntegrasyonApplication.java
@@ -0,0 +1,13 @@
+package com.example.pazaryerientegrasyon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PazarYeriEntegrasyonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PazarYeriEntegrasyonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pazaryerientegrasyon/PazarYeriEntegrasyonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pazaryerientegrasyon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PazarYeriEntegrasyonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
