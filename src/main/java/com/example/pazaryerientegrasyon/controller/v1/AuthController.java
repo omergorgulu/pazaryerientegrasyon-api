291395be85ef36a8ed0d1ba6157a0179415da6f5
Ali Emrah PEKESEN <aliemrahpekesen@gmail.com>
2021-11-29 15:51:03 +0300

    Bean declaration is added.

--- src/main/java/com/example/pazaryerientegrasyon/controller/v1/AuthController.java
+++ src/main/java/com/example/pazaryerientegrasyon/controller/v1/AuthController.java
@@ -2,16 +2,20 @@ package com.example.pazaryerientegrasyon.controller.v1;
 
 import com.example.pazaryerientegrasyon.domain.model.loginapi.LoginRequest;
 import com.example.pazaryerientegrasyon.domain.model.loginapi.LoginResponse;
+import com.example.pazaryerientegrasyon.service.AuthService;
 import com.example.pazaryerientegrasyon.service.impl.AuthServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
+    @Autowired
+    private AuthService authService;
+
     @PostMapping("/login")
     public LoginResponse login(@RequestBody LoginRequest request){
-        AuthServiceImpl authService = new AuthServiceImpl();
         return authService.login(request);
     }
 }
--- src/main/java/com/example/pazaryerientegrasyon/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/pazaryerientegrasyon/service/impl/AuthServiceImpl.java
@@ -3,11 +3,17 @@ package com.example.pazaryerientegrasyon.service.impl;
 import com.example.pazaryerientegrasyon.domain.model.loginapi.LoginRequest;
 import com.example.pazaryerientegrasyon.domain.model.loginapi.LoginResponse;
 import com.example.pazaryerientegrasyon.service.AuthService;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class AuthServiceImpl implements AuthService {
+
+    public AuthServiceImpl(){
+        System.out.println("Auth Service Impl Bean Tree ye girdi!");
+    }
 
     @Override
     public LoginResponse login(LoginRequest request) {
