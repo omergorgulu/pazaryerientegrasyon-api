583d3481ecbb54132e58265eb82a29cb40f5336c
ogorgulu <ogorgulu@partner.innova.com.tr>
2021-11-30 11:41:31 +0300

    update

---
+++ depInj/src/main/java/com/aep/training/depInj/DepInjApplication.java
@@ -0,0 +1,13 @@
+package com.aep.training.depInj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepInjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepInjApplication.class, args);
+    }
+
+}
---
+++ depInj/src/test/java/com/aep/training/depInj/DepInjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aep.training.depInj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepInjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
